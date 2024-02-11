6d5265f43e827c1f20732d036c4f3730fd5c6314
sviatj <asviatsukha@gmail.com>
2024-02-11 16:32:24 +0300

    feat: 10 pass / 9 left

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -242,8 +242,13 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  for (let i = 0; i < str.length / 2; i += 1) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
